a80097407a99f51aba9359b3bea332679e751d82
Kikekike <38482128+Kikekike@users.noreply.github.com>
2021-08-05 18:50:15 -0500

    Tips component Finished (#18)
    * :construction: TipComponent
    
    Adding TipComponent
    
    * :construction: Tips component fixed
    
    Tips component fixed
    
    Co-authored-by: Ian Pedraza <48818116+IanPedraza@users.noreply.github.com>

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -16,4 +16,6 @@ const Map<int, Color> _primaryColor = {
 };
 
 const colorTextMainButton = Colors.white;
+const colorTextBlack = Colors.black;
+const colorLight = Colors.amber;
 const colorAnswerOptionCard = Color.fromARGB(255, 154, 138, 247);
