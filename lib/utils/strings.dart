58b87b8714a604262593fe757cf44986a977276d
fermorav <fer.1998.madrid@gmail.com>
2021-08-13 18:12:11 -0500

    feedback requested changes

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -25,6 +25,7 @@ const button_finish = "Terminar";
 const button_reset = "Restablecer Contraseña";
 const button_accept = "Aceptar";
 const button_item_test = "Test Item";
+const button_cancel = "Cancelar";
 
 //Etiquetas
 const text_forget_password = "¿Olvidaste tu Contraseña?";
