94fcec63495a3cdcba4be915b1d78f96362101ec
fermorav <85763773+fermorav@users.noreply.github.com>
2021-09-16 18:37:49 -0500

    get test (#41)
    * change-befero-merge
    
    * fixed requested changes
    
    * requested changes
    
    * feedback requested changes
    
    * main changes
    
    * main-changes
    
    * test-page-second-changes
    
    * test-page-second-changes
    
    * test-home-section-changes
    
    * test-page-changes
    
    Co-authored-by: Ian Pedraza <48818116+IanPedraza@users.noreply.github.com>

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -16,6 +16,22 @@ class QuestionPage extends StatefulWidget {
 
 class _QuestionPageState extends State<QuestionPage> {
   late Test test;
+  
+  int currentIndex = 0;
+  PageController _controller = PageController(initialPage: 0);
+  bool choosen = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     _getArguments();
@@ -32,13 +48,86 @@ class _QuestionPageState extends State<QuestionPage> {
             padding: EdgeInsets.only(left: 15, top: 0, right: 15, bottom: 30),
             alignment: Alignment.center,
             child: Text(
-              'ID: ${test.id}',
+              test.description,
               style: TextStyle(
                 color: primaryColor,
                 fontWeight: FontWeight.bold,
-                fontSize: 20,
+                fontSize: 12,
               ),
               textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height * 0.65,
+              minHeight: 100,
+            ),
+            child: PageView.builder(
+              controller: _controller,
+              scrollDirection: Axis.horizontal,
+              itemCount: test.questions.length,
+              onPageChanged: (int index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              itemBuilder: (context, int index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          test.questions.length,
+                          (index) => buildDot(index),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(
+                          left: 15, top: 0, right: 15, bottom: 30),
+                      alignment: Alignment.center,
+                      child: Text(
+                        test.questions[index].question,
+                        style: TextStyle(
+                          color: primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: test.questions[index].options.length,
+                        itemBuilder: (context, int indx) {
+                          return Container(
+                            padding: EdgeInsets.only(
+                                left: 15, top: 10, right: 15, bottom: 10),
+                            child: AnswerOptionCard(
+                              onPressed: _setChoosen,
+                              textAnswer:
+                                  test.questions[index].options[indx].name,
+                              choosen: choosen,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 15, top: 10, right: 15, bottom: 15),
+            child: MainButton(
+              onPressed: _nextPage,
+              text: currentIndex == test.questions.length - 1
+                  ? button_finish
+                  : button_next,
             ),
           ),
         ],
@@ -53,5 +142,29 @@ class _QuestionPageState extends State<QuestionPage> {
       return;
     }
     test = args[test_args];
+  }
+
+  void _nextPage() {
+    _controller.nextPage(
+        duration: Duration(milliseconds: 100), curve: Curves.bounceIn);
+  }
+
+  void _setChoosen() {
+    setState(() {
+      choosen = !choosen;
+    });
+  }
+
+  Container buildDot(int index) {
+    return Container(
+      height: 10,
+      width: 10,
+      margin: EdgeInsets.only(right: 5, bottom: 30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: primaryColor, width: 1),
+        color: currentIndex == index ? primaryColor : Colors.transparent,
+      ),
+    );
   }
 }
