db480244557b275cda7457dccb8796aee22a1a47
fermorav <fer.1998.madrid@gmail.com>
2021-09-18 21:43:11 -0500

    changes-before-merge

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -1,9 +1,22 @@
 import 'package:dietari/components/AnswerOptionCard.dart';
 import 'package:dietari/components/AppBarComponent.dart';
 import 'package:dietari/components/MainButton.dart';
+import 'package:dietari/components/ShowAlertDialog.dart';
+import 'package:dietari/data/datasources/AuthDataSource.dart';
+import 'package:dietari/data/datasources/UserDataSource.dart';
 import 'package:dietari/data/domain/Test.dart';
+import 'package:dietari/data/domain/UserOption.dart';
+import 'package:dietari/data/domain/UserQuestion.dart';
+import 'package:dietari/data/domain/UserTest.dart';
+import 'package:dietari/data/framework/FireBase/FirebaseAuthDataSource.dart';
+import 'package:dietari/data/framework/FireBase/FirebaseUserDataSouce.dart';
+import 'package:dietari/data/repositories/AuthRepository.dart';
+import 'package:dietari/data/repositories/UserRepository.dart';
+import 'package:dietari/data/usecases/AddUserTestUseCase.dart';
+import 'package:dietari/data/usecases/GetUserIdUseCase.dart';
 import 'package:dietari/utils/arguments.dart';
 import 'package:dietari/utils/colors.dart';
+import 'package:dietari/utils/routes.dart';
 import 'package:dietari/utils/strings.dart';
 import 'package:flutter/material.dart';
 
@@ -15,10 +28,28 @@ class QuestionPage extends StatefulWidget {
 }
 
 class _QuestionPageState extends State<QuestionPage> {
+  late UserDataSource _userDataSource = FirebaseUserDataSouce();
+
+  late UserRepository _userRepository =
+      UserRepository(userDataSource: _userDataSource);
+
+  late AddUserTestUseCase _addUserTestUseCase =
+      AddUserTestUseCase(userRepository: _userRepository);
+
+  late AuthDataSource _authDataSource = FirebaseAuthDataSource();
+
+  late AuthRepository _authRepository =
+      AuthRepository(authDataSource: _authDataSource);
+
+  late GetUserIdUseCase _getUserIdUseCase =
+      GetUserIdUseCase(authRepository: _authRepository);
+
   late Test test;
+  late UserTest _userTest;
   int currentIndex = 0;
   PageController _controller = PageController(initialPage: 0);
-  bool choosen = false;
+  List<bool> choose = List.generate(10, (index) => false);
+  List<int> _options = List.generate(10, (index) => -1);
 
   @override
   void initState() {
@@ -34,6 +65,7 @@ class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
     _getArguments();
+    _createUserTest();
     return Scaffold(
       appBar: AppBarComponent(
         textAppBar: test.title,
@@ -47,7 +79,7 @@ class _QuestionPageState extends State<QuestionPage> {
             padding: EdgeInsets.only(left: 15, top: 0, right: 15, bottom: 30),
             alignment: Alignment.center,
             child: Text(
-              test.description,
+              _userTest.description,
               style: TextStyle(
                 color: primaryColor,
                 fontWeight: FontWeight.bold,
@@ -57,39 +89,35 @@ class _QuestionPageState extends State<QuestionPage> {
             ),
           ),
           Container(
+            padding: EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                _userTest.questions.length,
+                (index) => buildDot(index),
+              ),
+            ),
+          ),
+          Container(
             constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height * 0.65,
+              maxHeight: MediaQuery.of(context).size.height * 0.55,
               minHeight: 100,
             ),
             child: PageView.builder(
               controller: _controller,
+              physics: NeverScrollableScrollPhysics(),
               scrollDirection: Axis.horizontal,
-              itemCount: test.questions.length,
-              onPageChanged: (int index) {
-                setState(() {
-                  currentIndex = index;
-                });
-              },
-              itemBuilder: (context, int index) {
+              itemCount: _userTest.questions.length,
+              itemBuilder: (BuildContext context, int index) {
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                          test.questions.length,
-                          (index) => buildDot(index),
-                        ),
-                      ),
-                    ),
                     Container(
                       padding: EdgeInsets.only(
                           left: 15, top: 0, right: 15, bottom: 30),
                       alignment: Alignment.center,
                       child: Text(
-                        test.questions[index].question,
+                        _userTest.questions[index].question,
                         style: TextStyle(
                           color: primaryColor,
                           fontWeight: FontWeight.bold,
@@ -100,16 +128,18 @@ class _QuestionPageState extends State<QuestionPage> {
                     ),
                     Expanded(
                       child: ListView.builder(
-                        itemCount: test.questions[index].options.length,
-                        itemBuilder: (context, int indx) {
+                        itemCount: _userTest.questions[index].options.length,
+                        itemBuilder: (BuildContext context, int indx) {
                           return Container(
                             padding: EdgeInsets.only(
                                 left: 15, top: 10, right: 15, bottom: 10),
                             child: AnswerOptionCard(
-                              onPressed: _setChoosen,
+                              onPressed: () {
+                                _setChoosen(indx);
+                              },
                               textAnswer:
-                                  test.questions[index].options[indx].name,
-                              choosen: choosen,
+                                  _userTest.questions[index].options[indx].name,
+                              choosen: choose[indx],
                             ),
                           );
                         },
@@ -117,6 +147,11 @@ class _QuestionPageState extends State<QuestionPage> {
                     ),
                   ],
                 );
+              },
+              onPageChanged: (int index) {
+                setState(() {
+                  currentIndex = index;
+                });
               },
             ),
           ),
@@ -143,14 +178,72 @@ class _QuestionPageState extends State<QuestionPage> {
     test = args[test_args];
   }
 
-  void _nextPage() {
-    _controller.nextPage(
-        duration: Duration(milliseconds: 100), curve: Curves.bounceIn);
+  void _createUserTest() {
+    _userTest = new UserTest(
+      title: test.title,
+      description: test.description,
+      id: test.id,
+      isComplete: false,
+      questions: [],
+    );
+    for (int i = 0; i < test.questions.length; i++) {
+      UserQuestion _userQuestion =
+          new UserQuestion(question: test.questions[i].question, options: []);
+      for (int j = 0; j < test.questions[i].options.length; j++) {
+        UserOption _userOption = new UserOption(
+          name: test.questions[i].options[j].name,
+          value: test.questions[i].options[j].value,
+          isSelected: false,
+        );
+        _userQuestion.options.add(_userOption);
+      }
+      _userTest.questions.add(_userQuestion);
+    }
   }
 
-  void _setChoosen() {
+  void _nextPage() {
+    if (choose.contains(true)) {
+      if (currentIndex == _userTest.questions.length - 1) {
+        _saveAnswers();
+        _userTest.isComplete = true;
+        String? id = _getUserIdUseCase.invoke();
+        if (id != null) {
+          _addUserTest(id, _userTest).then(
+            (isDone) => {
+              if (isDone)
+                {Navigator.pushNamed(context, finished_test_route)}
+              else
+                {
+                  _showAlertDialog(context, alert_title_error,
+                      alert_content_not_aggregated_responses),
+                  Navigator.of(context).pop()
+                }
+            },
+          );
+        }
+      }
+      choose = List.generate(10, (i) => false);
+      _controller.nextPage(
+        duration: Duration(milliseconds: 100),
+        curve: Curves.bounceIn,
+      );
+    } else {
+      _showAlertDialog(context, alert_title, alert_content_answer_question);
+    }
+  }
+
+  void _saveAnswers() {
+    for (int i = 0; i < _userTest.questions.length; i++) {
+      _userTest.questions[i].options[_options[i]].isSelected = true;
+      print(_options[i]);
+    }
+  }
+
+  void _setChoosen(int index) {
+    choose = List.generate(10, (i) => false);
     setState(() {
-      choosen = !choosen;
+      choose[index] = !choose[index];
+      _options[currentIndex] = index;
     });
   }
 
@@ -165,5 +258,23 @@ class _QuestionPageState extends State<QuestionPage> {
         color: currentIndex == index ? primaryColor : Colors.transparent,
       ),
     );
+  }
+
+  void _showAlertDialog(BuildContext context, String title, String content) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return ShowAlertDialog(
+          title: title,
+          content: content,
+        );
+      },
+    );
+  }
+
+  Future<bool> _addUserTest(String userId, UserTest userTest) async {
+    bool addUser = await _addUserTestUseCase.invoke(userId, userTest);
+    return addUser;
   }
 }
