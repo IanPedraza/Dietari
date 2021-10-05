00c2594616cf702a9e7ed5068ccc4c992b61f4db
Ian Pedraza <48818116+IanPedraza@users.noreply.github.com>
2021-10-04 20:41:11 -0500

    :sparkles: Settings page (#60)
    The settings page has been implemented.

--- lib/components/MainTextField.dart
+++ lib/components/MainTextField.dart
@@ -3,49 +3,67 @@ import 'package:flutter/material.dart';
 import 'package:dietari/utils/colors.dart';
 import 'package:dietari/utils/icons.dart';
 
-class MainTextField extends StatelessWidget {
+class MainTextField extends StatefulWidget {
   final Function() onTap;
   final String text;
   final bool isPassword;
   final bool isPasswordTextStatus;
   final TextEditingController textEditingControl;
+  final bool isNumber;
+  final String? errorText;
 
-  const MainTextField(
-      {Key? key,
-      required this.text,
-      required this.isPassword,
-      required this.textEditingControl,
-      required this.isPasswordTextStatus,
-      required this.onTap})
-      : super(key: key);
+  const MainTextField({
+    Key? key,
+    required this.text,
+    required this.isPassword,
+    required this.textEditingControl,
+    required this.isPasswordTextStatus,
+    required this.onTap,
+    this.isNumber = false,
+    this.errorText,
+  }) : super(key: key);
+
+  @override
+  _MainTextFieldState createState() => _MainTextFieldState();
+}
+
+class _MainTextFieldState extends State<MainTextField> {
+  final _border = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide(color: primaryColor),
+  );
+
+  final _errorBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide(color: Colors.red),
+  );
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-        controller: textEditingControl,
+        keyboardType:
+            widget.isNumber ? TextInputType.number : TextInputType.text,
+        controller: widget.textEditingControl,
         autofocus: false,
         autocorrect: true,
         textAlign: TextAlign.left,
-        obscureText: isPasswordTextStatus,
+        obscureText: widget.isPasswordTextStatus,
         style: TextStyle(
             color: primaryColor, fontSize: 18, fontWeight: FontWeight.bold),
         decoration: InputDecoration(
-          labelText: text,
+          errorText: widget.errorText,
+          labelText: widget.text,
           labelStyle:
               TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: primaryColor),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: primaryColor),
-          ),
-          suffixIcon: isPassword
+          enabledBorder: _border,
+          focusedBorder: _border,
+          errorBorder: _errorBorder,
+          border: _border,
+          suffixIcon: widget.isPassword
               ? InkWell(
-                  onTap: isPassword ? onTap : null,
-                  child: (isPassword
-                      ? (isPasswordTextStatus
+                  onTap: widget.isPassword ? widget.onTap : null,
+                  child: (widget.isPassword
+                      ? (widget.isPasswordTextStatus
                           ? getIcon(AppIcons.visibility_off,
                               color: primaryColor)
                           : getIcon(AppIcons.visibility, color: primaryColor))
