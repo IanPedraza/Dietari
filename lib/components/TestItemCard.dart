bfdde6f55088b183d07c5886a8034f15d4d7916f
Ian Pedraza <ianpedrazacelon@gmail.com>
2021-10-28 18:43:27 -0500

    :bug: TestItemComponent bug
    The test item component bug has been solved

--- lib/components/TestItemCard.dart
+++ lib/components/TestItemCard.dart
@@ -19,39 +19,81 @@ class TestItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    double getResponsiveText(double size) =>
-      size * sizeReference / MediaQuery.of(context).size.longestSide; 
-
-    return FloatingActionButton(
-      onPressed: onPressed,
-      backgroundColor: backgroundColorTestItem,
-      elevation: 8,
-      isExtended: true,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.only(left: 15),
-              child: Expanded(
-                child: Text(
-                  textTestItem,
-                  style: TextStyle(
-                      color: colorTextMainButton,
-                      fontWeight: FontWeight.w900,
-                      fontSize: getResponsiveText(20)),
-                  textAlign: TextAlign.left,
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColorTestItem,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 3,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                textTestItem,
+                style: TextStyle(
+                  color: colorTextMainButton,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 20, right: 25),
-            child: check ? SvgPicture.asset(image_check, width: 40) : null,
-          ),
-        ],
+            check
+                ? SvgPicture.asset(
+                    image_check,
+                    width: 35,
+                  )
+                : Container(),
+          ],
+        ),
       ),
     );
+
+    // double getResponsiveText(double size) =>
+    //     size * sizeReference / MediaQuery.of(context).size.longestSide;
+
+    // return Material(
+    //   color: backgroundColorTestItem,
+    //   elevation: 8,
+    //   shape: RoundedRectangleBorder(
+    //     borderRadius: BorderRadius.circular(10),
+    //   ),
+    //   child: InkWell(
+    //     onTap: onPressed,
+    //     child: Row(
+    //       children: [
+    //         Expanded(
+    //           child: Container(
+    //             padding: const EdgeInsets.only(left: 15),
+    //             child: Expanded(
+    //               child: Text(
+    //                 textTestItem,
+    //                 style: TextStyle(
+    //                   color: colorTextMainButton,
+    //                   fontWeight: FontWeight.w900,
+    //                   fontSize: getResponsiveText(20),
+    //                 ),
+    //                 textAlign: TextAlign.left,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //         Container(
+    //           padding: const EdgeInsets.only(left: 20, right: 25),
+    //           child: check ? SvgPicture.asset(image_check, width: 40) : null,
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
